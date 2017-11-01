180be973db484df82b25d5c22b58c1b13a7a15d1
Brennan Butler <bbutler522@gmail.com>
2017-11-01 11:46:33 -0400

    Cache the upcoming image to avoid loading

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,6 +23,16 @@ Controller for the discover page
     // prepare the next song
     Recommendations.nextSong();
 
+    // used for retrieving the next album image.
+    // if there isn't an album image available next, return empty string.
+    $scope.nextAlbumImg = function() {
+      if (Recommendations.queue.length > 1) {
+        return Recommendations.queue[1].image_large;
+      }
+
+      return '';
+    }
+
     $timeout(function() {
       // $timeout to allow animation to complete
       $scope.currentSong = Recommendations.queue[0];
